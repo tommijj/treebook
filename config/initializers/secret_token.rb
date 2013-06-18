0b60afc8c3c9a5a4abb44f75ca9190ee4b94fadc
Tommi J„rvinen <tommi.jarvinen@b105.fi>
2013-06-18 22:45:28 +0300

    "testailua" exit
    n#	new file:   app/assets/stylesheets/statuses.css.scss
    	new file:   vendor/assets/stylesheets/.gitkeep
    	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '3f9db140c3662b306ee3433e618f002e9c1bfb4f2e046910284e5f6fecc20ac47bda38fd713da3481ca4e14ffbc0ab382cd0337db3c2108e998a8d2d124e65e6'
